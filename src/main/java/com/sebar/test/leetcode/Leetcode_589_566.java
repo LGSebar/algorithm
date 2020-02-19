83aff98c491160a17125b1fa9c43f40336f86945
liguang@youxin.com <liguang@youxin.com>
2020-02-19 17:44:35 +0800

    push in order travelsal

--- src/main/java/com/sebar/test/leetcode/Leetcode_589_566.java
+++ src/main/java/com/sebar/test/leetcode/Leetcode_589_566.java
@@ -1,6 +1,8 @@
 package com.sebar.test.leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -25,10 +27,33 @@ public class Leetcode_589_566 {
         }
     }
 
-    public List<Integer> preorder(Node root) {
-        List<Integer> resList = new ArrayList<>();
-        recursivePreOrder(root, resList);
-        return resList;
+    public static void main(String[] args) {
+
+        Leetcode_589_566.Node rootNode = new Leetcode_589_566.Node(1);
+
+        Leetcode_589_566.Node secondNode1 = new Leetcode_589_566.Node(3);
+        Leetcode_589_566.Node secondNode2 = new Leetcode_589_566.Node(2);
+        Leetcode_589_566.Node secondNode3 = new Leetcode_589_566.Node(4);
+
+        Leetcode_589_566.Node thirdNode1 = new Leetcode_589_566.Node(5);
+        Leetcode_589_566.Node thirdNode2 = new Leetcode_589_566.Node(6);
+
+        List<Leetcode_589_566.Node> nodeList = new ArrayList<>();
+        nodeList.add(secondNode1);
+        nodeList.add(secondNode2);
+        nodeList.add(secondNode3);
+
+        List<Leetcode_589_566.Node> nodeList2 = new ArrayList<>();
+        nodeList2.add(thirdNode1);
+        nodeList2.add(thirdNode2);
+
+        rootNode.children = nodeList;
+        secondNode1.children = nodeList2;
+
+        Leetcode_589_566 e = new Leetcode_589_566();
+//        List<Integer> integerList = e.preorder(rootNode);
+        List<Integer> integerList = e.preOrderWithStack(rootNode);
+        System.out.println(1);
     }
 
     /**
@@ -57,31 +82,45 @@ public class Leetcode_589_566 {
         return;
     }
 
-    public static void main(String[] args) {
+    public List<Integer> preorder(Node root) {
+        List<Integer> resList = new ArrayList<>();
+        if (root == null) {
+            return resList;
+        }
+        recursivePreOrder(root, resList);
+        return resList;
+    }
 
-        Leetcode_589_566.Node rootNode = new Leetcode_589_566.Node(1);
+    /**
+     * 栈的方式解决前置
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preOrderWithStack(Node root) {
+        LinkedList<Node> nodeStackList = new LinkedList<>();
+        List<Integer> resList = new ArrayList<>();
 
-        Leetcode_589_566.Node secondNode1 = new Leetcode_589_566.Node(3);
-        Leetcode_589_566.Node secondNode2 = new Leetcode_589_566.Node(2);
-        Leetcode_589_566.Node secondNode3 = new Leetcode_589_566.Node(4);
+        if (root == null) {
+            return resList;
+        }
 
-        Leetcode_589_566.Node thirdNode1 = new Leetcode_589_566.Node(5);
-        Leetcode_589_566.Node thirdNode2 = new Leetcode_589_566.Node(6);
+        // 先把跟加进去
+        nodeStackList.add(root);
+        while (!nodeStackList.isEmpty()) {
+            Node currentNode = nodeStackList.pollLast();
+            resList.add(currentNode.val);
 
-        List<Leetcode_589_566.Node> nodeList = new ArrayList<>();
-        nodeList.add(secondNode1);
-        nodeList.add(secondNode2);
-        nodeList.add(secondNode3);
+            // 获取左右子节点，压入栈中
+            if (currentNode.children != null) {
+                // 反转节点
+                Collections.reverse(currentNode.children);
 
-        List<Leetcode_589_566.Node> nodeList2 = new ArrayList<>();
-        nodeList2.add(thirdNode1);
-        nodeList2.add(thirdNode2);
-
-        rootNode.children = nodeList;
-        secondNode1.children = nodeList2;
-
-        Leetcode_589_566 e = new Leetcode_589_566();
-        List<Integer> integerList = e.preorder(rootNode);
-        System.out.println(1);
+                for (Node child : currentNode.children) {
+                    nodeStackList.add(child);
+                }
+            }
+        }
+        return resList;
     }
 }
