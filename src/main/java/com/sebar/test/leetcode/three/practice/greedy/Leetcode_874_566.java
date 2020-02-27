18a72e5fa3437d641cdc802b4425480cfc70b60c
= <=>
2020-02-27 23:52:38 +0800

    greedy

---
+++ src/main/java/com/sebar/test/leetcode/three/practice/greedy/Leetcode_874_566.java
@@ -0,0 +1,24 @@
+package com.sebar.test.leetcode.three.practice.greedy;
+
+/**
+ * 874. 模拟行走机器人
+ * https://leetcode-cn.com/problems/walking-robot-simulation/description/
+ */
+public class Leetcode_874_566 {
+    /**
+     * @param commands
+     * @param obstacles
+     * @return
+     */
+    public int robotSim(int[] commands, int[][] obstacles) {
+        if (commands == null) {
+            return 0;
+        }
+
+        int hengXiang = 0;
+        int zongXiang = 0;
+
+
+        return 0;
+    }
+}
