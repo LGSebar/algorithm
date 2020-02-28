ac15dcd0429cdd766681b636441fb346056f34ec
liguang@youxin.com <liguang@youxin.com>
2020-02-28 18:09:30 +0800

    add binary search2

---
+++ src/main/java/com/sebar/test/leetcode/three/homework/Leetcode_200_566.java
@@ -0,0 +1,15 @@
+package com.sebar.test.leetcode.three.homework;
+
+/**
+ * @author liguang
+ * @Date 2020/2/28
+ * @Description 200. 岛屿数量
+ * https://leetcode-cn.com/problems/number-of-islands/
+ */
+
+public class Leetcode_200_566 {
+
+    public int numIslands(char[][] grid) {
+        return 0;
+    }
+}
